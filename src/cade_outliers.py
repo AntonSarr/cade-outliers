9e674c185af1698ac410bdb54ee4d4df682a6a93
AntonSarr <anton.soroka.1313@yandex.by>
2024-05-07 09:30:32 +0400

    api and example change

--- src/cade_outliers.py
+++ src/cade_outliers.py
@@ -1,13 +1,10 @@
-import itertools
-
 import numpy as np
-from sklearn.model_selection import train_test_split
 
 
 class CADEOutliers():
     """Class for outliers detection with CADE (Classifier Adjusted Density Estimation)"""
     
-    _supported_A_dist = ["uniform", "grid"]
+    _supported_A_dist = ["uniform"]
     
     def __init__(self, classifier, A_dist, A_size):
         """Create CADE outlier detection object.
@@ -16,8 +13,10 @@ class CADEOutliers():
         ----------
         classifier : model
             sklearn classifier model supporting fit and predict_proba methods.
+        
         A_dist : string
-            Distribution for generating artificial anomalies. Can be "uniform" or "grid".
+            Distribution for generating artificial anomalies.
+        
         A_size : float | int
             If float, then the number of artifical anomalies is |X| * A_size, where X - analyzed data. 
             If int, then the number of artifical anomalies is just A_size.
@@ -45,25 +44,22 @@ class CADEOutliers():
         Parameters
         ----------
         dist : string
-            Distribution for generating artificial anomalies. Can be "uniform" or "grid".
+            Distribution for generating artificial anomalies.
         attrs : dict
             Key-value arguments for generating artifical anomalies. 
             If dist is "uniform", attrs = {'low': array, 'high': array, 'size': (sample_size, n_dim)} with min and high thresholds for each dimension.
-            If dist is "grid", attrs = {'low': array, 'high': array, 'num': array} with values for each dimension.
         """
         if dist == 'uniform':
             generator = np.random.uniform
-        elif dist == 'grid':
-            generator = lambda low, high, num: np.array([list(x) for x in itertools.product(*[np.linspace(start, stop, num) for start, stop, num in zip(low, high, num)])])
         else:
             raise ValueError(f"{dist} distribution is not supported yet for generating artifical anomalies. Choose from the following list: {self._supported_A_dist}")
         
         A = generator(**attrs)
         
         return A
-        
-    def outliers_ranking(self, X):
-        """Return an array for ranking outliers in X.
+    
+    def fit(self, X):
+        """Fit the distribution of X.
         
         Parameters
         ----------
@@ -88,35 +84,30 @@ class CADEOutliers():
                 if s == 0:
                     s = 1
                 P_A /= s
-            P_A = np.array([P_A] * X.shape[0])
-        elif self.A_dist == 'grid':
-            n_dim = X.shape[1]
-            attrs = {
-                'low': X.min(axis=0),
-                'high': X.max(axis=0),
-                'num': [int(pow(sample_size, 1 / n_dim)) + 1 for i in range(n_dim)]
-            }
-            # calculate uniform probability density
-            P_A = 1 
-            for s in X.max(axis=0) - X.min(axis=0):
-                if s == 0:
-                    s = 1
-                P_A /= s
-            P_A = np.array([P_A] * X.shape[0])
+            self.P_A = lambda x: np.array([P_A] * x.shape[0])
         else:
             attrs = None
             
         A = self._generate_A(self.A_dist, attrs)
         
-        combined_data = np.vstack([A, X])
+        combined_data = np.vstack([X, A])
         target = np.hstack([np.zeros(X.shape[0]), np.ones(A.shape[0])])
         
-        X_train, X_test, y_train, y_test = train_test_split(combined_data, target, test_size=0.33)
+        self.classifier.fit(combined_data, target)
+        self.A_X_ratio = A.shape[0] / X.shape[0]
         
-        self.classifier.fit(X_train, y_train)
         
+    def get_ranking(self, X):
+        """Return rankings for each sample in X.
+        Higher values indicate higher certainty that the point is an outlier.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Array with samples
+        """
         predictions = self.classifier.predict_proba(X)[:, 0]
         
-        anomaly_score = P_A * (A.shape[0] / X.shape[0]) * (predictions / (1 - predictions + 1e-5))
+        anomaly_score = self.P_A(X) * self.A_X_ratio * (predictions / (1 - predictions + 1e-5))
         
         return anomaly_score
